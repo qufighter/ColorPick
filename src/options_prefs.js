565dec4890b2ed5748b5fc56eddf226bb28ba701
Sam Larison <samlarison@gmail.com>
2018-12-19 13:30:30 -0600

    remove unused functions from content scripts

--- src/options_prefs.js
+++ src/options_prefs.js
@@ -47,52 +47,6 @@ pSyncItems["reg_hash"]={def:""};
 pSyncItems["reg_name"]={def:""};
 pSyncItems["reg_inapp"]={def:false};
 
-function sendReloadPrefs(cb){
-	var cbf=cb;
-	if(typeof(cbf)!='function')cbf=function(){};
-	chrome.runtime.sendMessage({reloadprefs: true}, function(response) {cbf()});
-}
-
-function chromeStorageSaveALocalStor(tosave){
-	storage.set(tosave, function() {
-		if(chrome.runtime.lastError && chrome.runtime.lastError.message.indexOf('MAX_WRITE_OPERATIONS_PER_HOUR') > 0){
-			//console.log(chrome.runtime.lastError);
-		}
-	});
-}
-
-function saveSyncItemsToChromeSyncStorage(){
-	var tosave={};
-	for(var i in pSyncItems){
-		tosave[i]=localStorage[i];
-	}
-	chromeStorageSaveALocalStor(tosave);
-}
-function saveToChromeSyncStorage(){
-	var tosave={};
-	for(var i in pOptions){
-		tosave[i]=localStorage[i];
-	}
-	for(var i in pAdvOptions){
-		tosave[i]=localStorage[i];
-	}
-	chromeStorageSaveALocalStor(tosave);
-}
-
-function loadSettingsFromChromeSyncStorage(cbf){
-	
-	storage.get(null, function(obj) {
-		for(i in obj){
-			if(pOptions[i] || pAdvOptions[i] || pSyncItems[i]){
-				localStorage[i] = obj[i];
-			}
-		}
-		sendReloadPrefs();
-		if(typeof(cbf)=='function')cbf();
-	});
-}
-
 function formatColorValues(a,b,c,pcta,pctb,pctc){
 	return CSS3ColorFormat.replace('#1',a/*+(pcta?'%':'')*/).replace('#2',b+(pctb?'%':'')).replace('#3',c+(pctc?'%':''));
 }
-
---
+++ src/options_prefs_helpers.js
@@ -0,0 +1,46 @@
+// these functions are NOT included in user.js (content scripts)
+
+function sendReloadPrefs(cb){
+	var cbf=cb;
+	if(typeof(cbf)!='function')cbf=function(){};
+	chrome.runtime.sendMessage({reloadprefs: true}, function(response) {cbf()});
+}
+
+function chromeStorageSaveALocalStor(tosave){
+	storage.set(tosave, function() {
+		if(chrome.runtime.lastError && chrome.runtime.lastError.message.indexOf('MAX_WRITE_OPERATIONS_PER_HOUR') > 0){
+			//console.log(chrome.runtime.lastError);
+		}
+	});
+}
+
+function saveSyncItemsToChromeSyncStorage(){
+	var tosave={};
+	for(var i in pSyncItems){
+		tosave[i]=localStorage[i];
+	}
+	chromeStorageSaveALocalStor(tosave);
+}
+function saveToChromeSyncStorage(){
+	var tosave={};
+	for(var i in pOptions){
+		tosave[i]=localStorage[i];
+	}
+	for(var i in pAdvOptions){
+		tosave[i]=localStorage[i];
+	}
+	chromeStorageSaveALocalStor(tosave);
+}
+
+function loadSettingsFromChromeSyncStorage(cbf){
+	
+	storage.get(null, function(obj) {
+		for(i in obj){
+			if(pOptions[i] || pAdvOptions[i] || pSyncItems[i]){
+				localStorage[i] = obj[i];
+			}
+		}
+		sendReloadPrefs();
+		if(typeof(cbf)=='function')cbf();
+	});
+}
