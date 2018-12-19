ae8ab4ea72fc96235da385193b2f6205b603a73d
Sam Larison <samlarison@gmail.com>
2018-12-19 15:17:32 -0600

    tweak styles, fix drag snapshot bug

--- src/colorpick.user.js
+++ src/colorpick.user.js
@@ -248,7 +248,7 @@ function initialInit(){
 
 function prefsLoadedCompleteInit(){
 	removeExistingNodes();
-	c=Cr.elm('img',{id:elmid1,src:blankgif,style:'position:fixed;max-width:none!important;max-height:none!important;top:0px;left:0px;margin:0px;padding:0px;overflow:hidden;z-index:2147483646;',events:[['click',picked,true],['load',snapshotLoaded]]},[],document.body);
+	c=Cr.elm('img',{id:elmid1,src:blankgif,style:'position:fixed;max-width:none!important;max-height:none!important;top:0px;left:0px;margin:0px;padding:0px;overflow:hidden;z-index:2147483646;',events:[['click',picked,true],['mousedown',function(ev){ev.preventDefault();}],['load',snapshotLoaded]]},[],document.body);
 	n=Cr.elm('div',{id:elmid2,style:'position:fixed;min-width:30px;max-width:300px;min-height:30px;box-shadow:2px 2px 2px #666;border:'+borderValue+';z-index:2147483646;cursor:default;padding:4px;'},[Cr.txt(' ')],document.body);
 	document.addEventListener('mousemove',mmf);
 	addEventListener('keyup',wk);
